be78622779052e8458a6cd1f87c6113439e7eb69
orifchik88 <orifchik88gmail.com>
2024-07-30 15:59:03 +0500

    asdf

--- app/Http/Requests/DxaResponseRegisterRequest.php
+++ app/Http/Requests/DxaResponseRegisterRequest.php
@@ -28,7 +28,7 @@ class DxaResponseRegisterRequest extends FormRequest
             'commit' => 'string|required',
             'long' => 'string|required',
             'lat' => 'required|required',
-            'object_status_id' => 'integer|required',
+            'administrative_status_id' => 'integer|required',
             'task_id' => 'integer|required|exists:dxa_responses,task_id',
             'images.*' => 'required|image|mimes:jpg,jpeg,png'
         ];
