bff41980039f2666243668a17b715b9c85b83406
Hayitboyevich <shhayitboyevich@gmail.com>
2024-09-10 18:44:35 +0500

    adsfasdf

--- config/app.php
+++ config/app.php
@@ -76,7 +76,7 @@ return [
 
     'gasn' => [
         'monitoring' => 'https://api.shaffofqurilish.uz/api/v1/request/monitoring-objects?ekspertiza_number',
-        'get_monitoring' => 'https://api.shaffofqurilish.uz/api/v1/request/monitoring-objects?gnk_id=',
+        'get_monitoring' => 'https://api.shaffofqurilish.uz/api/v1/request/monitoring-objects?gnk_id',
         'programs' => 'https://api.shaffofqurilish.uz/api/v1/request/monitoring-dasturlar',
         'reestr' => 'https://api.shaffofqurilish.uz/api/v1/request/opendata-reestr?reestr_number',
         'rating' => 'https://api.shaffofqurilish.uz/api/v1/request/reyting?INN',
