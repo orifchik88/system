e92a2f38fad8e10cda47ce5a52dfaf308bbcbd56
Hayitboyevich <shhayitboyevich@gmail.com>
2024-09-13 18:30:15 +0500

    asdf

--- database/migrations/2024_09_13_132813_change_gnk_id_to_articles_table.php
+++ database/migrations/2024_09_13_132813_change_gnk_id_to_articles_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('articles', function (Blueprint $table) {
-            $table->bigInteger('gnk_id')->nullable();
+            $table->bigInteger('gnk_id')->change();
         });
     }
 
