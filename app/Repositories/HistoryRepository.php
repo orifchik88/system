e6c32a93f13e34e28002b38cf6d5a1fe38a8dcaa
Dosia <nonameclubuz@gmail.com>
2024-10-02 17:21:44 +0500

    added history module

---
+++ app/Repositories/HistoryRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Enums\LogType;
+use App\Repositories\Interfaces\HistoryRepositoryInterface;
+use Illuminate\Support\Facades\DB;
+
+class HistoryRepository implements HistoryRepositoryInterface
+{
+    protected $table;
+
+    public function __construct($table)
+    {
+        $this->table = $table;
+    }
+
+    public function getHistoryList(int $guId)
+    {
+        return DB::table($this->table)->where('gu_id', $guId)->where('type', LogType::TASK_HISTORY)->orderBy('id', 'asc')->get([
+            'gu_id',
+            'content',
+            'created_at'
+        ]);
+    }
+
+    public function createHistory(int $guId, array $content, int $type)
+    {
+        return DB::table($this->table)->insert([
+            'gu_id' => $guId,
+            'content' => $content,
+            'type' => $type
+        ]);
+    }
+}
