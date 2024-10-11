096cd0d38198f4907dea2a6a17910cb686464612
Dosia <nonameclubuz@gmail.com>
2024-10-11 19:04:02 +0500

    fixed reviews

--- app/Models/ClaimOrganizationReview.php
+++ app/Models/ClaimOrganizationReview.php
@@ -8,11 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class ClaimOrganizationReview extends Model
 {
     use HasFactory;
+
     protected $table = 'claim_organization_reviews';
     protected $guarded = [];
 
     public function monitoring()
     {
         return $this->belongsTo(ClaimMonitoring::class, 'monitoring_id', 'id')->with('claim');
+    }
+
+    public function getAnswerAttribute()
+    {
+        $answers = json_decode(gzuncompress(base64_decode($this->attributes['answer'])), true);
+        foreach ($answers as $key => $value) {
+            if (str_contains($key, 'conclusion_act'))
+                $answers['conclusion_act'] = $value;
+        }
+        return $answers['conclusion_act'];
     }
 }
