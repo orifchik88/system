7a4c52009fba01b04e44759f4ad65c856a57d6c0
Dosia <nonameclubuz@gmail.com>
2024-10-11 19:36:06 +0500

    dd

--- app/Services/ClaimService.php
+++ app/Services/ClaimService.php
@@ -402,11 +402,11 @@ class ClaimService
         ];
 
         $claimObject = $this->getClaimById(id: $request['id'], role_id: null);
-        //$response = $this->PostRequest("update/id/" . $claimObject->gu_id . "/action/send-to-step-conclusion-gasn", $dataArray);
+        $response = $this->PostRequest("update/id/" . $claimObject->gu_id . "/action/send-to-step-conclusion-gasn", $dataArray);
 
-//        if ($response->status() != 200) {
-//            return false;
-//        }
+        if ($response->status() != 200) {
+            return false;
+        }
 
         $dataArray['IssuanceExtractRejectGasnV2FormCompletedBuildingsRegistrationCadastral'] = [
             "gasn_name_reject" => Auth::user()->name . ' ' . Auth::user()->surname,
