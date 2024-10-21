325a0318c90a0bfbf1f38c0226ae388e967ebb1e
Hayitboyevich <shhayitboyevich@gmail.com>
2024-10-21 12:47:20 +0500

    adf

--- app/Console/Commands/ChecklistAnswerAcceptCommand.php
+++ app/Console/Commands/ChecklistAnswerAcceptCommand.php
@@ -34,13 +34,13 @@ class ChecklistAnswerAcceptCommand extends Command
             ->where('status', CheckListStatusEnum::SECOND)
             ->chunk(50, function ($checklists) {
                 foreach ($checklists as $checklist) {
-
-                    echo "inspector foreachga kirdi";
                     $checklist->update([
                         'inspector_answered' => 1,
                         'technic_author_answered_at' => null,
                         'inspector_answered_at' => null,
                     ]);
+
+                    $this->saveHistory(UserRoleEnum::INSPECTOR->value, $checklist->id);
                 }
             });
         CheckListAnswer::query()->where('technic_author_answered_at', '<=', Carbon::now())
@@ -62,6 +62,9 @@ class ChecklistAnswerAcceptCommand extends Command
                             'author_answered' => 1,
                         ]);
                     }
+                    $this->saveHistory(UserRoleEnum::MUALLIF->value, $checklist->id);
+
+
 
                 }
             });
@@ -86,119 +89,28 @@ class ChecklistAnswerAcceptCommand extends Command
                         ]);
                     }
 
+                    $this->saveHistory(UserRoleEnum::TEXNIK->value, $checklist->id);
+
                 }
             });
     }
 
-//    public function handle()
-//    {
-//
-//        $this->processChecklistAnswers(
-//            'inspector_answered_at',
-//            CheckListStatusEnum::SECOND,
-//            function ($checklist) {
-//                $checklist->update([
-//                    'inspector_answered' => 1,
-//                    'status' => CheckListStatusEnum::CONFIRMED,
-//                    'technic_author_answered_at' => null,
-//                    'inspector_answered_at' => null,
-//                ]);
-//
-//                $object = Article::query()->find($checklist->object_id);
-//                $inspector = $object->users()->where('role_id', UserRoleEnum::INSPECTOR->value)->first();
-//
-//                $content =  [
-//                    'user' => $inspector->id ?? "",
-//                    'role' => UserRoleEnum::INSPECTOR->value,
-//                    'date' =>  now(),
-//                    'status' => $checklist->status->value,
-//                    'comment' => 'Automatik ravishda tasdiqlandi',
-//                    'additionalInfo' => ['user_answered' => 1, 'answered' => 'auto']
-//                ];
-//
-//                $this->saveTable($checklist->id, $content, LogType::TASK_HISTORY);
-//            }
-//        );
-//        $this->processChecklistAnswers(
-//            'technic_author_answered_at',
-//            CheckListStatusEnum::FIRST,
-//            function ($checklist) {
-//                if (is_null($checklist->technic_answered)) {
-//                    $checklist->update([
-//                        'author_answered' => 1,
-//                    ]);
-//                } else {
-//                    $checklist->update([
-//                        'author_answered' => 1,
-//                        'status' => CheckListStatusEnum::SECOND,
-//                        'technic_author_answered_at' => null,
-//                    ]);
-//                }
-//
-//                $object = Article::query()->find($checklist->object_id);
-//                $author = $object->users()->where('role_id', UserRoleEnum::MUALLIF->value)->first();
-//
-//                $content =  [
-//                    'user' => $author->id ?? "",
-//                    'role' => UserRoleEnum::MUALLIF->value,
-//                    'date' =>  now(),
-//                    'status' => $checklist->status->value,
-//                    'comment' => 'Automatik ravishda tasdiqlandi',
-//                    'additionalInfo' => ['user_answered' => 1, 'answered' => 'auto']
-//                ];
-//                $this->saveTable($checklist->id, $content, LogType::TASK_HISTORY);
-//
-//            },
-//            ['author_answered' => null]
-//        );
-//
-//        $this->processChecklistAnswers(
-//            'technic_author_answered_at',
-//            CheckListStatusEnum::FIRST,
-//            function ($checklist) {
-//                if (is_null($checklist->author_answered)) {
-//                    $checklist->update([
-//                        'author_answered' => 1,
-//                    ]);
-//                } else {
-//                    $checklist->update([
-//                        'author_answered' => 1,
-//                        'status' => CheckListStatusEnum::SECOND,
-//                        'technic_author_answered_at' => null,
-//                    ]);
-//                }
-//
-//                $object = Article::query()->find($checklist->object_id);
-//                $technic = $object->users()->where('role_id', UserRoleEnum::TEXNIK->value)->first();
-//
-//                $content =  [
-//                    'user' => $technic->id ?? "",
-//                    'role' => UserRoleEnum::TEXNIK->value,
-//                    'date' =>  now(),
-//                    'status' => $checklist->status->value,
-//                    'comment' => 'Automatik ravishda tasdiqlandi',
-//                    'additionalInfo' => ['user_answered' => 1, 'answered' => 'auto']
-//                ];
-//                $this->saveTable($checklist->id, $content, LogType::TASK_HISTORY);
-//            },
-//            ['technic_answered' => null]
-//        );
-//
-//        echo "bajarildi";
-//    }
+    private function saveHistory($roleId, $checklist)
+    {
+        $object = Article::query()->find($checklist->object_id);
+        $user = $object->users()->where('role_id', $roleId)->first();
 
-//    private function processChecklistAnswers($dateField, $status, $callback, $additionalConditions = [])
-//    {
-//        CheckListAnswer::query()
-//            ->where($dateField, '<=', Carbon::now())
-//            ->where('status', $status)
-//            ->where($additionalConditions)
-//            ->chunk(50, function ($checklists) use ($callback) {
-//                foreach ($checklists as $checklist) {
-//                    $callback($checklist);
-//                }
-//            });
-//    }
+        $content =  [
+            'user' => $user->id ?? "",
+            'role' => $roleId,
+            'date' =>  now(),
+            'status' => $checklist->status->value,
+            'comment' => 'Automatik ravishda tasdiqlandi',
+            'additionalInfo' => ['user_answered' => 1, 'answered' => 'auto']
+        ];
+
+        $this->saveTable($checklist->id, $content, LogType::TASK_HISTORY);
+    }
 
     private function saveTable($guId, $content, $type)
     {
