aebbb43b29b924d708495d9f601a833a9f648933
Hayitboyevich <shhayitboyevich@gmail.com>
2024-10-26 22:33:01 +0500

    asdf

--- app/Services/ArticleService.php
+++ app/Services/ArticleService.php
@@ -14,12 +14,16 @@ use App\Models\Article;
 use App\Models\ArticlePaymentLog;
 use App\Models\DxaResponse;
 use App\Models\FundingSource;
+use App\Models\User;
 use App\Models\UserEmployee;
+use App\Models\UserRole;
 use App\Repositories\Interfaces\ArticleRepositoryInterface;
 use App\Repositories\Interfaces\BlockRepositoryInterface;
 use App\Repositories\Interfaces\UserRepositoryInterface;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use PHPUnit\Exception;
 
 class ArticleService
 {
@@ -209,33 +213,197 @@ class ArticleService
     {
         return $this->articleRepository->getAccountObjectsQuery($query, $status);
     }
-
-
-    public function createObject(): Article
+    public function createObject()
     {
+
         DB::beginTransaction();
         try {
-            $response = $this->findResponse();
-            $this->updateResponse($response);
+            $response = $this->dxaResponse->find($this->objectDto->responseId);
+            $response->update([
+                'is_accepted' => true,
+                'confirmed_at' => now(),
+                'dxa_response_status_id' => DxaResponseStatusEnum::ACCEPTED
+            ]);
 
-            $article = $this->handleArticleCreationOrUpdate($response);
-            $this->handleArticleSupervisors($article, $response);
-            $this->attachInspector($article, $response->inspector_id);
+            if ($response->notification_type==2)
+            {
+                $article = Article::query()->where('task_id', $response->old_task_id)->first();
+                $article->update([
+                    'name' => $response->object_name,
+                    'region_id' => $response->region_id,
+                    'district_id' => $response->district_id,
+                    'object_type_id' => $response->object_type_id,
+                    'organization_name' => $response->organization_name,
+                    'location_building' => $response->location_building,
+                    'address' => $response->address,
+                    'construction_works' =>$response->construction_works,
+                    'cadastral_number' => $response->cadastral_number,
+                    'name_expertise' => $response->name_expertise,
+                    'difficulty_category_id' => DifficultyCategoryEnum::fromString($response->category_object_dictionary),
+                    'construction_cost' => $response->cost,
+                    'organization_projects' => $response->organization_projects,
+                    'specialists_certificates' => $response->specialists_certificates,
+                    'contract_file' => $response->contract_file,
+                    'confirming_laboratory' => $response->confirming_laboratory,
+                    'file_energy_efficiency' => $response->file_energy_efficiency,
+                    'legal_opf' => $response->legal_opf,
+                    'sphere_id' => $response->sphere_id,
+                    'program_id' => $response->program_id,
+                    'linear_type' => $response->linear_type,
+                    'dxa_response_id' => $response->id,
+                    'price_supervision_service' => price_supervision($response->cost),
+                    'task_id' => $response->task_id,
+                    'number_protocol' => $response->number_protocol,
+                    'positive_opinion_number' => $response->positive_opinion_number,
+                    'date_protocol' =>$response->date_protocol,
+                    'funding_source_id' => $response->funding_source_id,
+                    'deadline' => $response->end_term_work,
+                    'gnk_id' => $response->gnk_id,
+                    'reestr_number' => (int)$response->reestr_number,
+                ]);
+            }else{
+                $article = new Article();
+                $article->name = $response->object_name;
+                $article->region_id = $response->region_id;
+                $article->district_id = $response->district_id;
+                $article->object_status_id = ObjectStatusEnum::PROGRESS;
+                $article->object_type_id = $response->object_type_id;
+                $article->organization_name = $response->organization_name;
+                $article->location_building = $response->location_building;
+                $article->address = $response->address;
+                $article->cadastral_number = $response->cadastral_number;
+                $article->name_expertise = $response->name_expertise;
+                $article->difficulty_category_id = DifficultyCategoryEnum::fromString($response->category_object_dictionary);
+                $article->construction_cost = $response->cost;
+                $article->sphere_id  = $response->sphere_id;
+                $article->program_id  = $response->program_id;
+                $article->construction_works  = $response->construction_works;
+                $article->linear_type  = $response->linear_type;
+                $article->appearance_type_id = 1;
+                $article->is_accepted = true;
+                $article->organization_projects = $response->organization_projects;
+                $article->specialists_certificates = $response->specialists_certificates;
+                $article->contract_file = $response->contract_file;
+                $article->confirming_laboratory = $response->confirming_laboratory;
+                $article->file_energy_efficiency = $response->file_energy_efficiency;
+                $article->legal_opf = $response->legal_opf;
+                $article->lat = $response->lat;
+                $article->long = $response->long;
+                $article->dxa_response_id = $response->id;
+                $article->price_supervision_service = price_supervision($response->cost);
+                $article->task_id = $response->task_id;
+                $article->number_protocol = $response->number_protocol;
+                $article->positive_opinion_number = $response->positive_opinion_number;
+                $article->date_protocol = $response->date_protocol;
+                $article->funding_source_id = $response->funding_source_id;
+                $article->paid = 0;
+                $article->payment_deadline = Carbon::now();
+                $article->deadline = $response->end_term_work;
+                $article->gnk_id = $response->gnk_id;
+                $article->reestr_number = (int)$response->reestr_number;
+                $article->save();
+            }
 
-            $this->saveBlocks($response, $article);
 
 
-            $this->saveEmployee($article);
+            if ($response->notification_type==2)
+            {
+
+                $article->users()->detach();
+            }
+
+
+            foreach ($response->supervisors as $supervisor) {
+                $fish = $this->generateFish($supervisor->fish);
+                $user = User::where('pinfl', $supervisor->stir_or_pinfl)->first();
+                if ($user) {
+                    $user->update([
+                        'name' => $fish ? $fish[1] : null,
+                        'surname' => $fish ? $fish[0] : null,
+                        'middle_name' => $fish ? $fish[2] : null,
+                        'phone' => $supervisor->phone_number,
+                        'login' => $supervisor->passport_number,
+                        'organization_name' => $supervisor->organization_name,
+                        'password' => bcrypt($supervisor->stir_or_pinfl),
+                        'user_status_id' => UserStatusEnum::ACTIVE,
+                        'pinfl' => $supervisor->stir_or_pinfl,
+                        'identification_number' => $supervisor->identification_number,
+                    ]);
+
+
+                    $article->users()->attach($user->id, ['role_id' => $supervisor->role_id]);
+                    if (!$user->roles()->where('role_id', $supervisor->role_id)->exists())
+                        UserRole::query()->create([
+                            'user_id' => $user->id,
+                            'role_id' => $supervisor->role_id,
+                        ]);
+                }
+                if (!$user) {
+                    $user = User::create([
+                        'name' => $fish ? $fish[1] : null,
+                        'surname' => $fish ? $fish[0] : null,
+                        'middle_name' => $fish ? $fish[2] : null,
+                        'phone' => $supervisor->phone_number,
+                        'login' => $supervisor->passport_number,
+                        'organization_name' => $supervisor->organization_name,
+                        'password' => bcrypt($supervisor->stir_or_pinfl),
+                        'user_status_id' => UserStatusEnum::ACTIVE,
+                        'pinfl' => $supervisor->stir_or_pinfl,
+                        'identification_number' => $supervisor->identification_number,
+                    ]);
+                    $article->users()->attach($user->id, ['role_id' => $supervisor->role_id]);
+                    UserRole::query()->create([
+                        'user_id' => $user->id,
+                        'role_id' => $supervisor->role_id,
+                    ]);
+                }
+            }
+
+
+            $article->users()->attach($response->inspector_id, ['role_id' => 3]);
 
             $this->acceptResponse($response);
+            $this->saveBlocks($response, $article);
+            $this->saveEmployee($article);
+
 
             DB::commit();
+
             return $article;
-        } catch (\Exception $exception) {
+
+        } catch (Exception $exception) {
             DB::rollBack();
-            throw new \Exception("Error creating object: " . $exception->getMessage());
+            throw new NotFoundException($exception->getLine(), $exception->getCode(), );
         }
+
     }
+
+
+//    public function createObject(): Article
+//    {
+//        DB::beginTransaction();
+//        try {
+//            $response = $this->findResponse();
+//            $this->updateResponse($response);
+//
+//            $article = $this->handleArticleCreationOrUpdate($response);
+//            $this->handleArticleSupervisors($article, $response);
+//            $this->attachInspector($article, $response->inspector_id);
+//
+//            $this->saveBlocks($response, $article);
+//
+//
+//            $this->saveEmployee($article);
+//
+//            $this->acceptResponse($response);
+//
+//            DB::commit();
+//            return $article;
+//        } catch (\Exception $exception) {
+//            DB::rollBack();
+//            throw new \Exception("Error creating object: " . $exception->getMessage());
+//        }
+//    }
 
     private function findResponse(): DxaResponse
     {
