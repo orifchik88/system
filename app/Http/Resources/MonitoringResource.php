a2827aaf775d4086826e57c46de5802c377fb1dc
Hayitboyevich <shhayitboyevich@gmail.com>
2024-12-13 17:03:03 +0500

    regulation add role

--- app/Http/Resources/MonitoringResource.php
+++ app/Http/Resources/MonitoringResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use App\Models\RegulationViolation;
+use App\Models\Role;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -24,6 +25,9 @@ class MonitoringResource extends JsonResource
         }
         $uniqueViolationCount = array_unique($violationCount);
 
+        $roles = Role::all()->keyBy('id');
+        $firstRegulation = $this->regulations()->first();
+
         return [
             'id' => $this->id,
             'object_name' => $this?->object?->name,
@@ -33,7 +37,8 @@ class MonitoringResource extends JsonResource
             'active_regulation_count' => $this->regulations()->where('regulation_status_id', '!=', 6)->count(),
             'violation_count' =>  count($uniqueViolationCount) ?? 0,
             'checklists' => ChecklistResource::collection($this->checklists),
-            'created_at' => $this->created_at
+            'created_at' => $this->created_at,
+            'role_name' => $roles[$firstRegulation->created_by_role_id]->name ?? '',
         ];
     }
 }
