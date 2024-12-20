730ff2af26e2d1ec7fca1a2445eb7dbff234ae61
Hayitboyevich <shhayitboyevich@gmail.com>
2024-12-20 13:50:01 +0500

    create response bug fix cron

--- app/Services/DxaLinearResponseService.php
+++ app/Services/DxaLinearResponseService.php
@@ -168,14 +168,18 @@ class DxaLinearResponseService
         try {
             if ($dxa->notification_type == 2) {
                 $response = DxaResponse::query()->where('task_id', $dxa->old_task_id)->first();
-                $reestrNumber = isset($dxa->reestr_number) ? $response->reestr_number : $dxa->reestr_number;
-                $dxa->update([
-                    'reestr_number' => $reestrNumber,
-                    'sphere_id' => $response->sphere_id,
-                    'monitoring_object_id' => $response->monitoring_object_id,
-                    'end_term_work' => $response->end_term_work,
-                    'administrative_status_id' => $response->administrative_status_id,
-                ]);
+                if ($response)
+                {
+                    $reestrNumber = isset($dxa->reestr_number) ? $response->reestr_number : $dxa->reestr_number;
+                    $dxa->update([
+                        'reestr_number' => $reestrNumber,
+                        'sphere_id' => $response->sphere_id,
+                        'monitoring_object_id' => $response->monitoring_object_id,
+                        'end_term_work' => $response->end_term_work,
+                        'administrative_status_id' => $response->administrative_status_id,
+                    ]);
+                }
+
             } else {
                 $reestrNumber = $dxa->reestr_number;
             }
