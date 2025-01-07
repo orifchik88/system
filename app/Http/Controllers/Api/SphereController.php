b722af3cc28297d883cac68a9322b41aa86258e5
Hayitboyevich <shhayitboyevich@gmail.com>
2025-01-07 11:50:34 +0500

    bug fix sphere list

--- app/Http/Controllers/Api/SphereController.php
+++ app/Http/Controllers/Api/SphereController.php
@@ -13,7 +13,11 @@ class SphereController extends BaseController
     public function spheres(): JsonResponse
     {
         try {
-            $spheres = Sphere::query()->paginate(request('per_page', 10));
+            $spheres = Sphere::query()
+                ->when(\request('name'), function ($query, $name) {
+                     $query->where('name_uz', 'like', '%' . $name . '%');
+                })
+                ->paginate(request('per_page', 10));
             return $this->sendSuccess(SphereResource::collection($spheres), 'All Spheres', pagination($spheres));
         }catch (\Exception $exception){
             return $this->sendError($exception->getMessage());
