738d93366155aa9c479ac2d38867cb9a7a62507c
Dosia <nonameclubuz@gmail.com>
2025-01-16 11:40:14 +0500

    Latest changes

--- app/Models/Claim.php
+++ app/Models/Claim.php
@@ -41,7 +41,7 @@ class Claim extends Model
     public function object(): BelongsTo
     {
         return $this->belongsTo(Article::class, 'object_id', 'id')->with(['region', 'district', 'users'])
-            ->select('id', 'name', 'location_building', 'region_id', 'district_id', 'lat', 'long', 'object_type_id', 'cost');
+            ->select('id', 'name', 'location_building', 'region_id', 'district_id', 'lat', 'long', 'object_type_id');
     }
 
     public function reviews(): HasMany
