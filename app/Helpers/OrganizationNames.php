897ac1cf8b38b1adb75911fad4fa7dfab1540eca
Dosia <nonameclubuz@gmail.com>
2025-01-17 17:26:16 +0500

    Latest changes

--- app/Helpers/OrganizationNames.php
+++ app/Helpers/OrganizationNames.php
@@ -10,6 +10,7 @@ class OrganizationNames
         18 => "Ozbekiston nogironlar assotsiatsiyasi {region} bo'limi",
         17 => "{region} nogironlar jamiyati",
         19 => "{region} qurilish va uy-joy kommunal xo'jaligi sohasida hududiy nazorat inspeksiyasi (ko'p kvartirali uy-joylarni boshqarish bo'limi)",
+        34 => "{region} Ekologiya va atrof-muhitni muhofaza qilish hududiy boshqarmasi",
     ];
 
 }
