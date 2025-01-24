e002ac3d3e2ef4cf7853764ba5000a60e05e1321
Hayitboyevich <shhayitboyevich@gmail.com>
2025-01-24 18:07:35 +0500

    asdfasdf

--- app/Console/Commands/ObjectUpdateCommand.php
+++ app/Console/Commands/ObjectUpdateCommand.php
@@ -15,7 +15,7 @@ class ObjectUpdateCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'app:object-update-command';
+    protected $signature = 'app:object-update-command {task_id}';
 
     /**
      * The console command description.
@@ -31,14 +31,15 @@ class ObjectUpdateCommand extends Command
     {
         try {
              Article::query()
-                ->select('id', 'task_id', 'reestr_number')
-                ->whereNotNull('reestr_number')
-                ->whereRaw("reestr_number ~ '^[0-9]+$'")
-                ->whereRaw("CAST(reestr_number AS BIGINT) > 200000")
-                ->whereRaw("CAST(reestr_number AS BIGINT) < 300000")
-                ->where('created_at', '>=', '2024-01-01 00:00:00')
-                ->whereNotNull('old_id')
-                ->where('is_change', false)
+                 ->where('task_id', $this->argument('task_id'))
+//                ->select('id', 'task_id', 'reestr_number')
+//                ->whereNotNull('reestr_number')
+//                ->whereRaw("reestr_number ~ '^[0-9]+$'")
+//                ->whereRaw("CAST(reestr_number AS BIGINT) > 200000")
+//                ->whereRaw("CAST(reestr_number AS BIGINT) < 300000")
+//                ->where('created_at', '>=', '2024-01-01 00:00:00')
+//                ->whereNotNull('old_id')
+//                ->where('is_change', false)
                 ->chunk(10, function ($articles) {
                     DB::transaction(function () use ($articles) {
                         foreach ($articles as $article) {
