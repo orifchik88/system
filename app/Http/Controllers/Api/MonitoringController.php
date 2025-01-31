13de5083673580df23a1a45a7f48b618109dc5f2
Hayitboyevich <shhayitboyevich@gmail.com>
2025-01-31 16:48:37 +0500

    sdfgsdfg

--- app/Http/Controllers/Api/MonitoringController.php
+++ app/Http/Controllers/Api/MonitoringController.php
@@ -55,7 +55,7 @@ class MonitoringController extends BaseController
     {
         try {
             $query = $this->monitoringService->getMonitorings($this->user, $this->roleId);
-            $filters = request()->only(['object_name','start_date', 'end_date', 'region_id', 'district_id', 'funding_source', 'category', 'task_id']);
+            $filters = request()->only(['object_name','start_date', 'role_id', 'end_date', 'region_id', 'district_id', 'funding_source', 'category', 'task_id']);
 
             $monitorings = $this->monitoringService->searchMonitoring($query, $filters)
                 ->orderBy('created_at', request('sort_by_date', 'DESC'))
