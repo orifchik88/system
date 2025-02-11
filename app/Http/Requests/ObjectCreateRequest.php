3e2e5cda6d6aeb46a61a8b56cf7331770bf07d93
Hayitboyevich <shhayitboyevich@gmail.com>
2025-02-11 12:10:43 +0500

    bug fix

--- app/Http/Requests/ObjectCreateRequest.php
+++ app/Http/Requests/ObjectCreateRequest.php
@@ -44,7 +44,7 @@ class ObjectCreateRequest extends FormRequest
             'manual_task_id' => 'required|string',
             'number_protocol' => 'required|integer',
             'funding_source_id' => 'required|integer|exists:funding_sources,id',
-            'linear_type' => 'required_if:object_type_id,1|string|exists:object_types,id',
+            'linear_type' => 'required_if:object_type_id,1|string',
             'sphere_id' => 'required|integer|exists:spheres,id',
             'users.*' => 'required|array',
             'blocks.*' => 'required|array',
