dd5fa2b6e1ddbf633b2fb69fa2b7a1a3c95cb634
Hayitboyevich <shhayitboyevich@gmail.com>
2025-04-11 09:57:04 +0500

    ketting najim

--- app/Enums/NotificationTypeEnum.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Enums;
-
-enum NotificationTypeEnum: int
-{
-    case DEVICE = 1;
-    case TELEGRAM = 2;
-}
--- app/Helpers/ApiType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Helpers;
-
-enum ApiType: string
-{
-    case MY_GOV_UZ = "my_gov_uz";
-    case LICENSE_GOV_UZ = "license_gov_uz";
-}
